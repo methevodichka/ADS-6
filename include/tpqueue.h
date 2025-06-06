763b53826be0904935f2b8f451bdc97bfb4dbaa8
methevodichka <145225062+methevodichka@users.noreply.github.com>
2025-06-06 14:02:00 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,60 +5,60 @@
 
 template<typename T>
 class TPQueue {
-    private:
-     struct Node {
-       T data;
-       Node* next;
-       explicit Node(const T &d, Node* n = nullptr) : data(d), next(n) { }
-      };
-      Node* head;
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T &d, Node* n = nullptr) : data(d), next(n) { }
+    };
+    Node* head;
 
-    public:
-     TPQueue() : head(nullptr) { }
-     ~TPQueue() {
-       while (!empty()) {
-       pop();
-       }
-     }
-      bool empty() const {
+ public:
+    TPQueue() : head(nullptr) { }
+    ~TPQueue() {
+        while (!empty()) {
+            pop();
+        }
+    }
+    bool empty() const {
         return head == nullptr;
-      }
-      void push(const T& value) {
+    }
+    void push(const T& value) {
         Node* newNode = new Node(value);
-          if (!head || value.prior > head->data.prior) {
+        if (!head || value.prior > head->data.prior) {
             newNode->next = head;
             head = newNode;
             } else {
-              Node* current = head;
-             while (current->next && current->next->data.prior >= value.prior) {
-                current = current->next;
+                Node* current = head;
+                while (current->next && current->next->data.prior >= value.prior) {
+                    current = current->next;
+                }
+                newNode->next = current->next;
+                current->next = newNode;
               }
-              newNode->next = current->next;
-              current->next = newNode;
-              }
-      }
-      T pop() {
+    }
+    T pop() {
         if (empty()) {
-          throw std::underflow_error("Pop out of empty queue");
+            throw std::underflow_error("Pop out of empty queue");
         }
         Node* temp = head;
         T value = head->data;
         head = head->next;
         delete temp;
         return value;
-      }
-      T& front() {
+    }
+    T& front() {
         if (empty()) {
-          throw std::underflow_error("Cast to empty queue");
+            throw std::underflow_error("Cast to empty queue");
         }
         return head->data;
-      }
-      const T& front() const {
+    }
+    const T& front() const {
         if (empty()) {
-          throw std::underflow_error("Cast to empty queue");
+            throw std::underflow_error("Cast to empty queue");
         }
         return head->data;
-      }
+    }
 };
 struct SYM {
   char ch;
